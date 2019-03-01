de6e2c30f4a55afcdfcb943ab2354cecb0aface9
Will Olson <will.olson11@gmail.com>
2019-02-28 19:18:22 -0800

    Convert to sass

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.css'
-import '@fortawesome/fontawesome-free/css/all.css'
 
 import todoApp from './reducers'
 import routes from './routes'
-import './index.css'
+import './index.scss'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(todoApp, composeEnhancers(
